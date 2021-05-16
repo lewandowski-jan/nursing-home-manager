8577f7786139536bbb35bc8bb828cbca443cf7a6
Jan Lewandowski <iasiu@icloud.com>
2021-05-17 01:18:48 +0200

    NHMDJ-2 added oracle server database credentials as default

--- src/nursinghomemanager/nursinghomemanager/settings.py
+++ src/nursinghomemanager/nursinghomemanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import cx_Oracle
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'nursinghomemanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'jlewand5',
+        'PASSWORD': 'jlewand5',
+        'HOST': 'ora3.elka.pw.edu.pl',
+        'PORT': '1521',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
