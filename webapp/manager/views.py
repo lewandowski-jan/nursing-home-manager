e8772ead36f8429a423622334a25e12c8d05b85a
Jan Lewandowski <iasiu@icloud.com>
2021-05-31 19:54:19 +0200

    added login/logout func to caretaker, manager

--- webapp/manager/views.py
+++ webapp/manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.template.defaulttags import register
+from django.contrib.auth.decorators import user_passes_test
 
 import datetime
 
@@ -9,11 +10,16 @@ from database.models import *
 def get_item(dictionary, key):
     return dictionary.get(key)
 
+def must_be_manager(user):
+    return user.groups.filter(name="Manager").count()
+
+@user_passes_test(must_be_manager, login_url='/login')
 def workers(request):
     workers = Pracownicy.objects.all()
     context = {'workers': workers}
     return render(request, 'manager_workers.html', context)
 
+@user_passes_test(must_be_manager, login_url='/login')
 def seniors(request):
     seniors = Seniorzy.objects.all()
     seniors_rooms = {}
@@ -26,17 +32,19 @@ def seniors(request):
     context = {'seniors': seniors, 'seniors_rooms': seniors_rooms}
     return render(request, 'manager_seniors.html', context)
 
-
+@user_passes_test(must_be_manager, login_url='/login')
 def workers(request):
     workers = Pracownicy.objects.all()
     context = {'workers': workers}
     return render(request, 'manager_workers.html', context)
 
+@user_passes_test(must_be_manager, login_url='/login')
 def medicines(request):
     medicines = Leki.objects.all()
     context = {'medicines': medicines}
     return render(request, 'manager_medicines.html', context)
 
+@user_passes_test(must_be_manager, login_url='/login')
 def medicines_add(request, id):
     if request.POST:
         medicine = Leki.objects.get(id=id)
