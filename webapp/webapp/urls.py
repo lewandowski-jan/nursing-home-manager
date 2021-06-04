1faca1be09bf092fd7cb145f4c911310c593b19d
Oskar Zając <zoskar12@gmail.com>
2021-06-04 20:47:27 +0200

    small fixes

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -12,3 +12,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
 ]
+
+admin.site.site_header = 'Administracja'
+
